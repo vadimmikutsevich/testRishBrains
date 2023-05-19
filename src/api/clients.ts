c1eb5b05f25275c66297caba3dff2e8999e05571
vadimmikutsevich <vadim.mikutsevich@gmail.com>
2023-05-19 11:25:36 +0400

    added cathing errors

--- src/api/clients.ts
+++ src/api/clients.ts
@@ -1,27 +1,78 @@
 import axios from 'axios';
 import { Client } from '../models';
 
+const API_URL = 'http://localhost:3333';
+
 export const getClients = async (): Promise<Client[]> => {
-  const response = await axios.get<Client[]>('http://localhost:3333/clients');
-  return response.data;
+  try {
+    const response = await axios.get<Client[]>(`${API_URL}/clients`);
+    return response.data;
+  } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(`Could not fetch clients: ${error.message}`);
+      } else {
+        throw new Error(`Could not fetch clients: ${error}`);
+      }
+  }
 };
 
 export const getClient = async (id: string): Promise<Client> => {
-  const response = await axios.post<Client>('http://localhost:3333/clients/get', { id });
-  return response.data;
+  try {
+    const response = await axios.post<Client>(`${API_URL}/clients/get`, { id });
+    return response.data;
+  } catch (error) {
+    if(error instanceof Error) {
+      throw new Error(`Could not fetch client with id ${id}: ${error.message}`);
+    } else {
+      throw new Error(`Could not fetch client with id ${id}: ${error}`);
+    }
+  }
 };
 
 export const addClient = async (client: Client): Promise<Client> => {
-  const response = await axios.post<Client>('http://localhost:3333/clients/add', client);
-  return response.data;
+  try {
+    const response = await axios.post<Client>(`${API_URL}/clients/add`, client);
+    if (response.status !== 201) {
+      throw new Error(`Unexpected response code: ${response.status}`);
+    }
+    return response.data;
+  } catch (error) {
+    if(error instanceof Error) {
+      throw new Error(`Could not add client: ${error.message}`);
+    } else {
+      throw new Error(`Could not add client: ${error}`);
+    }
+  }
 };
 
 export const editClient = async (client: Client): Promise<Client> => {
-  const response = await axios.put<Client>('http://localhost:3333/clients/edit', client);
-  return response.data;
+  try {
+    const response = await axios.put<Client>(`${API_URL}/clients/edit`, client);
+    if (response.status !== 200) {
+      throw new Error(`Unexpected response code: ${response.status}`);
+    }
+    return response.data;
+  } catch (error) {
+    if(error instanceof Error){
+      throw new Error(`Could not edit client: ${error.message}`);
+    } else {
+      throw new Error(`Could not edit client: ${error}`);
+    }
+  }
 };
 
 export const removeClient = async (id: string): Promise<string> => {
-  await axios.delete(`http://localhost:3333/clients/remove?id=${id}`);
-  return id;
+  try {
+    const response = await axios.delete(`${API_URL}/clients/remove?id=${id}`);
+    if (response.status !== 200) {
+      throw new Error(`Unexpected response code: ${response.status}`);
+    }
+    return id;
+  } catch (error) {
+    if(error instanceof Error){
+      throw new Error(`Could not remove client with id ${id}: ${error.message}`);
+    } else {
+      throw new Error(`Could not remove client with id ${id}: ${error}`);
+    }
+  }
 };
--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,20 @@
 import axios from 'axios';
 import { User, UserLoginPayload } from '../models';
 
+const API_URL = 'http://localhost:3333';
+
 export const loginUser = async (payload: UserLoginPayload): Promise<User> => {
-  const response = await axios.post<User>('http://localhost:3333/user/login', payload);
-  return response.data;
+  try {
+    const response = await axios.post<User>(`${API_URL}/user/login`, payload);
+    if (response.status !== 200) {
+      throw new Error(`Unexpected response code: ${response.status}`);
+    }
+    return response.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Could not login user: ${error.message}`);
+    } else {
+      throw new Error(`Could not login user: ${error}`);
+    }
+  }
 };
