ce2536adbf6a0faa0c23d9d5c92bc0bc376cd1d2
vadimmikutsevich <vadim.mikutsevich@gmail.com>
2023-05-20 20:53:23 +0400

    updated client cards

--- src/components/ClientCard.tsx
+++ src/components/ClientCard.tsx
@@ -1,6 +1,10 @@
-import React, {FC, memo} from "react";
-import { Card } from "antd";
-import { Client } from "../models";
+import React, {FC, memo} from "react"
+import { Card } from "antd"
+import {IoLocationOutline, IoPhonePortraitOutline, IoCalendarClearOutline} from 'react-icons/io5'
+import { Client } from "../models"
+import styles from '../styles/components/clientCard.module.css'
+import avatar from '../assets/ava.png'
+import { rightIconMargin } from "../styles/inLineStyles"
 
 interface UserCardProps {
     client: Client
@@ -8,10 +12,30 @@ interface UserCardProps {
 
 const ClientCard: FC<UserCardProps> = memo(function({client}) {
     return (
-        <Card title={client.name + ' ' + client.surname} bordered={false}>
-            <p>Age: {client.age}</p>
-            <p>Phone: {client.phone}</p>
-            <p>Country: {client.country}</p>
+        <Card bordered={false} className={styles.card}>
+            <img src={avatar} alt="avatar" className={styles.avatar}/>
+
+            <p className={styles.name}>
+                {client.name + ' ' + client.surname}
+            </p>
+            <p className={styles.country}>
+                <IoLocationOutline size={24} style={rightIconMargin} color="#87898F"/>
+                <span>
+                    {client.country}
+                </span>
+            </p>
+            <p className={styles.phone}>
+                <IoPhonePortraitOutline size={24} style={rightIconMargin} color="#87898F"/>
+                <span>
+                    {client.phone}
+                </span>
+            </p>
+            <p className={styles.age}>
+                <IoCalendarClearOutline size={24} style={rightIconMargin} color="#87898F"/>
+                <span>
+                    {client.age} y.o
+                </span>
+            </p>
         </Card>
     )
 })
